c3a1f23c762ae520b904fb36d95bc3d0f1e65e93
juac08 <juni430@gmail.com>
2021-03-29 01:35:26 +0200

    sgd

--- src/components/animation.js
+++ src/components/animation.js
@@ -3,21 +3,21 @@ import lottie from 'lottie-web';
 
 function Example() {
 
-  // const container = useRef(null);
-  // useEffect(() => {
-  //   lottie.loadAnimation({
-  //     container: container.current,
-  //     renderer: 'svg',
-  //     loop: true,
-  //     autoplay: true,
-  //     animationData: require('./mask.json'),
-  //   })
-  // }, [])
+  const container = useRef(null);
+  useEffect(() => {
+    lottie.loadAnimation({
+      container: container.current,
+      renderer: 'svg',
+      loop: true,
+      autoplay: true,
+      animationData: require('./mask.json'),
+    })
+  }, [])
   return (
     <div className='head'>
       <div className='container bg'>
       <h1 className='box b1'>Covid Tracker</h1>
-      {/* <div className="box b2" ref={container}></div> */}
+      <div className="box b2" ref={container}></div>
       
       </div>
       </div>
